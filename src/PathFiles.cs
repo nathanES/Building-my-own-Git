ed6f027467d1c2a78577e1d3ba4ec326c0accb93
nathan <nathan.esslinger67@gmail.com>
2024-08-31 15:58:25 +0200

    change path

--- src/PathFiles.cs
+++ src/PathFiles.cs
@@ -2,6 +2,8 @@ namespace codecrafters_git;
 
 public static class PathFiles
 {
-   public const string PATH_TO_GIT_FOLDER = "../../../.git/";
+   // public const string PATH_TO_GIT_FOLDER = "../../../.git/";
+   public const string PATH_TO_GIT_FOLDER = ".git/";
+
    public const string PATH_TO_GIT_OBJECTS_FOLDER = PATH_TO_GIT_FOLDER + "objects/";
 }
